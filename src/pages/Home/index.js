9a929dc7a38a8a78f8326e85d67fb3147ab8b250
LORDBABUINO <jorge.x7@gmail.com>
2019-12-19 05:16:50 -0300

    Prints professors names

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { Container } from './styles'
+import api from '../../services/api'
 
 export default function Home() {
+  const [professors, setProfessors] = useState([])
+  useEffect(() => {
+    ;(async () => {
+      const { data } = await api.get('/professors')
+      setProfessors(data)
+    })()
+  }, [])
+  console.log({ professors })
   return (
-		<Container>
-		</Container>
+    <Container>
+      <ul>
+        {professors.map(({ objectId, nome }) => (
+          <li key={objectId}>{nome}</li>
+        ))}
+      </ul>
+    </Container>
   )
 }
